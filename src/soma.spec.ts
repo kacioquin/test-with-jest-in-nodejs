727525224c6a6c2c259780b172c7d93e0285bca0
Cassio de Freitas <cassio.quintino@gmail.com>
2021-11-12 15:22:23 -0300

    Adicionando mais testes...

--- src/soma.spec.ts
+++ src/soma.spec.ts
@@ -1,5 +1,19 @@
 import soma from './soma'
 
-test("adds 1 + 2 to equal 3", () => {
-  expect(soma(1,2)).toBe(3);
+describe("Somar dois numeros", () => {
+  
+  it("Espero 2 + 2 seja igual a 4", () => {
+    const soma = 2 + 2;
+    const resultado = 4;
+
+    expect(soma).toBe(resultado);
+  });
+
+  it("Espero que 2 + 2 NÃO seja igual a 5", () => {
+    const soma = 2 + 2;
+    const resultado = 5;
+
+    expect(soma).not.toBe(5);
+  })
+
 })
